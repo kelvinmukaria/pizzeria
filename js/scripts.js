7fa725689d141d9ebf51aaa586559c0ef664dbda
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-25 17:47:21 +0300

    Add functions that collect user input for crust,size,toppings and no of pizzas in the backend

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,24 @@ $(document).ready(function(){
        var flavor = document.getElementById("s1").value;
        return parseInt(flavor);
    }
+   
+   function getPizzaSize(){
+    var size = document.getElementById("s2").value;
+    return parseInt(size);
+   }
+
+   function getPizzaCrust(){
+    var crust = document.getElementById("s3").value;
+    return parseInt(crust);
+   }
+
+   function getPizzaTopping(){
+    var topping = document.getElementById("s4").value;
+    return parseInt(topping);
+   }
+
+   function getPizzaNumber(){
+    var number = document.getElementById("s5").value;
+    return parseInt(number);
+   }
 });
