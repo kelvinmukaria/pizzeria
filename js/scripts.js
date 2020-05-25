45769348d922cff57949fe4414467c09a1754a30
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-25 18:04:16 +0300

    Add Javascript function to calculate total price of ordered items upon checkout

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,29 @@ $(document).ready(function(){
    }
    
    function getPizzaSize(){
-    var size = document.getElementById("s2").value;
-    return parseInt(size);
+       var size = document.getElementById("s2").value;
+       return parseInt(size);
    }
 
    function getPizzaCrust(){
-    var crust = document.getElementById("s3").value;
-    return parseInt(crust);
+       var crust = document.getElementById("s3").value;
+       return parseInt(crust);
    }
 
    function getPizzaTopping(){
-    var topping = document.getElementById("s4").value;
-    return parseInt(topping);
+       var topping = document.getElementById("s4").value;
+       return parseInt(topping);
    }
 
    function getPizzaNumber(){
     var number = document.getElementById("s5").value;
     return parseInt(number);
+   }
+
+   function totalAmount(){
+       var total = getPizzaSize() + getPizzaTopping() + getPizzaFlavor() + getPizzaCrust() *getPizzaNumber();
+       alert(
+           "You have ordered " + getPizzaFlavor("")
+       )
    }
 });
