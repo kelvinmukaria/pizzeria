1320b1b079776e93a271d1f1069eadeae21626e8
kelvinmukaria <mukariakelvin17@gmail.com>
2020-05-27 10:38:22 +0300

    Add form whereby user inputs their name,number and location should they want their order delivered, implement its functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
    function getPizzaFlavor(){
        var flavor = document.getElementById("s1").value;
        return parseInt(flavor);
@@ -25,14 +24,27 @@ $(document).ready(function(){
    }
 
    function totalAmount(){
-       var total = getPizzaSize() + getPizzaTopping() + getPizzaFlavor() + getPizzaCrust() *getPizzaNumber();
+       event.preventDefault();
+       var total = (getPizzaSize() + getPizzaTopping() + getPizzaFlavor() + getPizzaCrust() *getPizzaNumber());
+       console.log(total);
        alert(
-           "You have ordered " + getPizzaNumber("") + "and the total amount is " + totalAmount + "." + " It was a pleasure to serve and thank you for choosing Pizza Africana as your Outlet of choice. Thank you and welcome again!!! "
-       );
+           "You have ordered " + getPizzaNumber() + "and the total amount is " + totalAmount + "." + " It was a pleasure to serve and thank you for choosing Pizza Africana as your Outlet of choice. Thank you and welcome again!!! "
+       )
 
     var location = prompt("If you want your pizza to be delivered to you please enter your location and if not ignore this message and press enter.");
       if (location !== "") {
          alert("You order will be delivered shortly.Delivery fee is 100/= . If  you preferred to eat in kindly come for your order.");
        }    
    }
+
+$(document).ready(function () {
+    $("#delivery").submit(function () {
+
+        var name = $("input#name").val();
+        var number = $("input#number").val();
+        var location = $("input#location").val();
+
+        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
+    });
+    
 });
